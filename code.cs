3eb55f60eba18d790d3274133d0db99708732529
George Axel <162681576+friectiondolla@users.noreply.github.com>
2024-03-08 09:59:38 +0200

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,31 @@
+   public class IvyProfileAsset : ScriptableObject
+    { 
+        #if UNITY_EDITOR
+        public IvyProfile ivyProfile;
+
+        [ContextMenu("Reset to Default Ivy Settings")]
+        public void ResetProfile() {
+            ivyProfile.ResetSettings();
+            this.SetDirty();
+        }
+        #endif
+    }
+
+public class ObjExport
+	{
+		private static int StartIndex = 0;
+		
+		public static string MeshToString(MeshFilter mf, Transform t) 
+		{	
+			int numVertices = 0;
+			Mesh m = mf.sharedMesh;
+			if (!m)
+			{
+				return "####Error####";
+			}
+			Material[] mats = mf.GetComponent<Renderer>().sharedMaterials;
+			
+			StringBuilder sb = new StringBuilder();
+			
+			foreach(Vector3 vv in m.vertices)
+			{
